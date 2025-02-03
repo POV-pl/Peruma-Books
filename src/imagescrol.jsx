bc154f6d7aa45b5de4ca84feebc9179b60bc05f4
GouthamKP3002 <kpgoutham16@gmail.com>
2025-02-03 14:17:56 +0530

    imagescrol

--- src/imagescrol.jsx
+++ src/imagescrol.jsx
@@ -3,16 +3,12 @@ import MobileImage1 from "./assests/1.png";
 import MobileImage2 from "./assests/2.png";
 import MobileImage3 from "./assests/3.png";
 import MobileImage4 from "./assests/4.png";
-import LargeImage1 from "./assests/large1.png";
-import LargeImage2 from "./assests/large2.png";
-import LargeImage3 from "./assests/large3.png";
-import LargeImage4 from "./assests/large4.png";
 
 const ImageCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const mobileImages = [MobileImage1, MobileImage2, MobileImage3, MobileImage4];
-  const largeImages = [LargeImage1, LargeImage2, LargeImage3, LargeImage4];
+  // const largeImages = [LargeImage1, LargeImage2, LargeImage3, LargeImage4];
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -24,7 +20,7 @@ const ImageCarousel = () => {
 
   return (
     <div
-      className="relative w-full min-h-[250px] sm:h-[400px] md:h-[500px] lg:h-[600px] 
+      className="relative w-full min-h-[250px] sm:h-[400px] md:h-[600px] lg:h-[800px] 
                     max-h-[80vh] 
                     overflow-hidden 
                     border-y-2 border-orange-600"
@@ -39,7 +35,7 @@ const ImageCarousel = () => {
           }`}
         >
           <picture>
-            <source media="(min-width: 1024px)" srcSet={largeImages[index]} />
+            <source media="(min-width: 1024px)" srcSet={mobileImages[index]} />
             <img
               src={image}
               alt={`Slide ${index + 1}`}
